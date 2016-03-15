a7513ea1c562922a41f8e70ccdb2e4e0b0a8de0d
jfourmond <jerome@fourmond.fr>
2016-03-15 13:59:03 +0100

    Merge 2

--- graphe.h
+++ graphe.h
@@ -6,10 +6,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
-<<<<<<< HEAD
-=======
 #include <ctime>
->>>>>>> 519644312f8c69484c5d3594f90f9c7dd5b800fc
 
 #include "loader.h"
 #include "city.h"
@@ -20,10 +17,7 @@ class Graph {
     int nb_city;
     std::vector<City> cities;
     float ** distances;
-<<<<<<< HEAD
-=======
     static int * configuration;
->>>>>>> 519644312f8c69484c5d3594f90f9c7dd5b800fc
     
   public:
     Graph();
@@ -56,18 +50,10 @@ class Graph {
 
     friend std::ostream& operator<<(std::ostream& out, const Graph & g) { return g.print(out); }
     
-<<<<<<< HEAD
-=======
-    void load_instance(char * filename);
-    
->>>>>>> 519644312f8c69484c5d3594f90f9c7dd5b800fc
     void build_matrix();
     
     float compute_distance(City A, City B);
     
-<<<<<<< HEAD
-    
-=======
     /** AUTRES METHODES **/
     
     /*
@@ -80,7 +66,6 @@ class Graph {
      * Evalue la configuration
      */
     float eval() const;
->>>>>>> 519644312f8c69484c5d3594f90f9c7dd5b800fc
 };
 
 #endif
